9b4768af04cff9ad49f1bc42a015fa6d0cf3fa46
Sunless <burbliskirill@gmail.com>
2026-05-12 13:07:19 +0300

    0.2.3 Доп информация в профиле + миграция

--- Energy/Energy/Models/User.cs
+++ Energy/Energy/Models/User.cs
@@ -13,5 +13,10 @@ namespace Energy.Models
         public string PasswordHash {get; set;}
         public string Email { get; set;}
 
+
+        public string FirstName { get; set; }   
+        public string LastName { get; set; }    
+        public string Phone { get; set; }     
+
     }
 }
--- Energy/Energy/Pages/ProfilePage.xaml.cs
+++ Energy/Energy/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Energy.Data;
+using Energy.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,68 @@ namespace Energy.Pages
     /// </summary>
     public partial class ProfilePage : Page
     {
+        private bool _isEditing = false;
+
         public ProfilePage()
         {
             InitializeComponent();
+            LoadUserData();
+            SetEditMode(false);
+        }
+
+        private void LoadUserData()
+        {
+            using (var db = new AppDbContext())
+            {
+                var user = db.Users.Find(Session.CurrentUserId);
+                if (user != null)
+                {
+                    txtLogin.Text = user.Login;
+                    txtFirstName.Text = user.FirstName ?? "";
+                    txtLastName.Text = user.LastName ?? "";
+                    txtPhone.Text = user.Phone ?? "";
+                    txtEmail.Text = user.Email ?? "";
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                var user = db.Users.Find(Session.CurrentUserId);
+                if (user != null)
+                {
+                    user.FirstName = txtFirstName.Text.Trim();
+                    user.LastName = txtLastName.Text.Trim();
+                    user.Phone = txtPhone.Text.Trim();
+                    user.Email = txtEmail.Text.Trim();
+
+                    db.SaveChanges();
+                }
+            }
+
+            SetEditMode(false);
+            MessageBox.Show("Данные сохранены!", "Успех",
+                          MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            SetEditMode(true);
+        }
+
+        private void SetEditMode(bool isEditing)
+        {
+            _isEditing = isEditing;
+
+            txtFirstName.IsEnabled = isEditing;
+            txtLastName.IsEnabled = isEditing;
+            txtPhone.IsEnabled = isEditing;
+            txtEmail.IsEnabled = isEditing;
+
+            btnSave.Visibility = isEditing ? Visibility.Visible : Visibility.Collapsed;
+            btnEdit.Visibility = isEditing ? Visibility.Collapsed : Visibility.Visible;
         }
     }
 }
